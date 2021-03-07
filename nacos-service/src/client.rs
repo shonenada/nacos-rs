9be435297c47a2343bf17f2fcf32c1f8798a3a5b
shonenada <shonenada@gmail.com>
2021-03-07 19:12:49 +0800

    feat(nacos-service): add some impls and examples(query_instance, register_instance)

--- nacos-service/src/client.rs
+++ nacos-service/src/client.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 
-use crate::structs::{RegisterInstance, UnregistryInstance};
+use crate::structs::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct CommonResponse<T> {
@@ -43,15 +43,15 @@ impl NacosService {
         )
     }
 
-    pub async fn registry_instance(&self, args: &RegisterInstance) -> Result<String> {
+    pub async fn register_instance(&self, args: &RegisterInstance) -> Result<()> {
         let url = self.make_url("/v1/ns/instance");
         let client = reqwest::Client::new();
-        let resp = client.post(&url).form(&args).send().await?;
+        let resp = client.post(&url).form(args).send().await?;
         let status = resp.status();
         match status {
             StatusCode::OK => {
                 let body: String = resp.text().await?;
-                if body == "true" {
+                if body == "ok" {
                     Ok(())
                 } else {
                     Err(anyhow!(
@@ -65,7 +65,7 @@ impl NacosService {
             _ => {
                 let body = resp.text().await?;
                 Err(anyhow!(
-                    "Failed to request {}, status coode is {}, body: {}l",
+                    "Failed to request {}, status coode is {}, body: {}",
                     url,
                     status,
                     body
@@ -74,10 +74,10 @@ impl NacosService {
         }
     }
 
-    pub async fn unregistry_instance(&self, args: &UnregistryInstance) -> Result {
+    pub async fn deregister_instance(&self, args: &DeregistryInstance) -> Result<()> {
         let url = self.make_url("/v1/ns/instance");
         let client = reqwest::Client::new();
-        let resp = client.delete(&url).form(&args).send().await?;
+        let resp = client.delete(&url).form(args).send().await?;
         let status = resp.status();
         match status {
             StatusCode::OK => {
@@ -103,5 +103,83 @@ impl NacosService {
                 ))
             }
         }
+    }
+
+    pub async fn modify_instance(&self, args: &ModifyInstance) -> Result<()> {
+        let url = self.make_url("/v1/ns/instance");
+        let client = reqwest::Client::new();
+        let resp = client.put(&url).form(args).send().await?;
+        let status = resp.status();
+        match status {
+            StatusCode::OK => {
+                let body: String = resp.text().await?;
+                if body == "true" {
+                    Ok(())
+                } else {
+                    Err(anyhow!(
+                        "Failed to request {}, status code is {}, body: {}",
+                        url,
+                        status,
+                        body
+                    ))
+                }
+            }
+            _ => {
+                let body = resp.text().await?;
+                Err(anyhow!(
+                    "Failed to request {}, status code is {}, body: {}",
+                    url,
+                    status,
+                    body
+                ))
+            }
+        }
+    }
+
+    // TODO: FIXME String
+    pub async fn query_instance(&self, args: &QueryInstance) -> Result<String> {
+        let url = self.make_url("/v1/ns/instance/list");
+        let client = reqwest::Client::new();
+        let resp = client.get(&url).query(args).send().await?;
+        let status = resp.status();
+        match status {
+            StatusCode::OK => {
+                let result: String = resp.text().await?;
+                Ok(result)
+            }
+            _ => {
+                let body = resp.text().await?;
+                Err(anyhow!(
+                    "Failed to request {}, status code is {}, body: {}",
+                    url,
+                    status,
+                    body
+                ))
+            }
+        }
+    }
+
+    pub async fn query_instance_detail(&self, args: &QueryInstanceDetail) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn send_instance_beat(&self, args: &SendInstanceBeat) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn create_service(&self, args: &CreateService) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn update_service(&self, args: &UpdateService) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn delete_service(&self, args: &DeleteService) -> Result<()> {
+        Ok(())
+    }
+
+    pub async fn query_service(&self, args: &QueryService) -> Result<()> {
+        Ok(())
     }
 }
--- nacos-service/src/structs.rs
+++ nacos-service/src/structs.rs
@@ -1,27 +1,144 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+type Weight = f32;
+
+#[derive(Builder, Debug, Deserialize, Serialize)]
+#[builder(setter(into))]
 pub struct RegisterInstance {
     ip: String,
     port: u16,
+    #[serde(rename = "namepsaceId")]
     namespace_id: Option<String>,
-    weight: f32,
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    weight: Weight,
     enabled: bool,
     healthy: bool,
     metadata: Option<String>,
+    #[serde(rename = "clusterName")]
     cluster_name: Option<String>,
-    service_name: String,
+    #[serde(rename = "groupName")]
     group_name: Option<String>,
     ephemeral: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct UnregistryInstance {
+pub struct DeregistryInstance {
+    #[serde(rename = "serviceName")]
     service_name: String,
+    #[serde(rename = "groupName")]
     group_name: Option<String>,
     ephemeral: bool,
     ip: String,
     port: u16,
+    #[serde(rename = "clusterName")]
     cluster_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ModifyInstance {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    ephemeral: bool,
+    ip: String,
+    port: u16,
+    #[serde(rename = "clusterName")]
+    cluster_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    weight: Weight,
+    enabled: bool,
+    metadata: String,
+}
+
+#[derive(Builder, Debug, Serialize, Deserialize)]
+#[builder(setter(into))]
+pub struct QueryInstance {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    clusters: String,
+    #[serde(rename = "healthyOnly")]
+    healthy_only: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryInstanceDetail {
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    ephemeral: bool,
+    ip: String,
+    port: String,
+    cluster: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SendInstanceBeat {
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    beat: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateService {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    #[serde(rename = "protectThreshold")]
+    protect_threshold: f32,
+    #[serde(rename = "metadata")]
+    metadata: String,
+    selector: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteService {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateService {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    #[serde(rename = "namespaceId")]
+    namespace_id: String,
+    #[serde(rename = "protectThreshold")]
+    protect_threshold: f32,
+    metadata: String,
+    selector: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryService {
+    #[serde(rename = "serviceName")]
+    service_name: String,
+    #[serde(rename = "groupName")]
+    group_name: String,
+    #[serde(rename = "namespaceId")]
     namespace_id: String,
 }
